8d11fc50ccc05d1f0e6c19424440e17e4d72e18a
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2020-01-06 17:07:54 +0200

    (FACT-2155) Add legacy facts

--- spec/facter/facts/windows/memory/system/available_spec.rb
+++ spec/facter/facts/windows/memory/system/available_spec.rb
@@ -2,14 +2,21 @@
 
 describe 'Windows MemorySystemAvailable' do
   context '#call_the_resolver' do
-    it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'memory.system.available', value: '1.0 KiB')
-      allow(Facter::Resolvers::Memory).to receive(:resolve).with(:available_bytes).and_return(1024)
-      allow(Facter::ResolvedFact).to receive(:new).with('memory.system.available', '1.0 KiB').and_return(expected_fact)
+    let(:value) { '1.0 KiB' }
+    let(:expected_resolved_fact) { double(Facter::ResolvedFact, name: 'memory.system.available', value: value) }
+    let(:resolved_legacy_fact) { double(Facter::ResolvedFact, name: 'memoryfree', value: value, type: :legacy) }
+    subject(:fact) { Facter::Windows::MemorySystemAvailable.new }
 
-      fact = Facter::Windows::MemorySystemAvailable.new
-      expect(Facter::BytesToHumanReadable.convert(1024)).to eq('1.0 KiB')
-      expect(fact.call_the_resolver).to eq(expected_fact)
+    before do
+      expect(Facter::Resolvers::Memory).to receive(:resolve).with(:available_bytes).and_return(1024)
+      expect(Facter::BytesToHumanReadable.convert(1024)).to eq(value)
+      expect(Facter::ResolvedFact).to receive(:new).with('memory.system.available', value)
+                                                   .and_return(expected_resolved_fact)
+      expect(Facter::ResolvedFact).to receive(:new).with('memoryfree', value, :legacy).and_return(resolved_legacy_fact)
+    end
+
+    it 'returns free memory fact' do
+      expect(fact.call_the_resolver).to eq([expected_resolved_fact, resolved_legacy_fact])
     end
   end
 end
--- spec/facter/facts/windows/memory/system/total_spec.rb
+++ spec/facter/facts/windows/memory/system/total_spec.rb
@@ -2,14 +2,21 @@
 
 describe 'Windows MemorySystemTotal' do
   context '#call_the_resolver' do
-    it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'memory.system.total', value: '1.0 KiB')
-      allow(Facter::Resolvers::Memory).to receive(:resolve).with(:total_bytes).and_return(1024)
-      allow(Facter::ResolvedFact).to receive(:new).with('memory.system.total', '1.0 KiB').and_return(expected_fact)
+    let(:value) { '1.0 KiB' }
+    let(:expected_resolved_fact) { double(Facter::ResolvedFact, name: 'memory.system.total', value: value) }
+    let(:resolved_legacy_fact) { double(Facter::ResolvedFact, name: 'memorysize', value: value, type: :legacy) }
+    subject(:fact) { Facter::Windows::MemorySystemTotal.new }
 
-      fact = Facter::Windows::MemorySystemTotal.new
-      expect(Facter::BytesToHumanReadable.convert(1024)).to eq('1.0 KiB')
-      expect(fact.call_the_resolver).to eq(expected_fact)
+    before do
+      expect(Facter::Resolvers::Memory).to receive(:resolve).with(:total_bytes).and_return(1024)
+      expect(Facter::BytesToHumanReadable.convert(1024)).to eq(value)
+      expect(Facter::ResolvedFact).to receive(:new).with('memory.system.total', value)
+                                                   .and_return(expected_resolved_fact)
+      expect(Facter::ResolvedFact).to receive(:new).with('memorysize', value, :legacy).and_return(resolved_legacy_fact)
+    end
+
+    it 'returns memory size fact' do
+      expect(fact.call_the_resolver).to eq([expected_resolved_fact, resolved_legacy_fact])
     end
   end
 end
